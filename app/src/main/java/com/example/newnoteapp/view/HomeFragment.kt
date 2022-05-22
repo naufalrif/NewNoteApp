589a27da2b579edea757a6cabd4255345e6c6d17
Naufal <nhabibie25@gmail.com>
2022-05-22 22:05:46 +0700

    Added notes recyclerview on home fragment

--- app/src/main/java/com/example/newnoteapp/view/HomeFragment.kt
+++ app/src/main/java/com/example/newnoteapp/view/HomeFragment.kt
@@ -6,13 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.asLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newnoteapp.R
 import com.example.newnoteapp.adapter.NotesAdapter
 import com.example.newnoteapp.datastore.UserManager
+import com.example.newnoteapp.notes.NotesDB
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
+    var dbNotes : NotesDB? = null
     lateinit var notesAdapter : NotesAdapter
     lateinit var userManager: UserManager
      override fun onCreateView(
@@ -30,5 +35,23 @@ class HomeFragment : Fragment() {
             tv_welcomeuser.text = "Welcome, $it!"
         }
 
+        getNoteData()
+
+    }
+
+    fun getNoteData(){
+        rv_notes.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        GlobalScope.launch {
+            val listData = dbNotes?.notesDao()?.getNoteData()
+            activity?.runOnUiThread {
+                if(listData?.size!! < 1){
+                    title_home.setText("No notes yet!")
+                }
+                listData.let {
+                    rv_notes.adapter = NotesAdapter(it!!)
+                }
+            }
+        }
     }
 }
